6e291a5dd5fbc36cc5b22f66b35bd5262f1e2422
TranNgocSon1996 <transon821996@gmail.com>
2021-01-20 12:02:55 +0700

    [ADD] tns_football_manager: manager the football

---
+++ tns_football_management/__manifest__.py
@@ -0,0 +1,56 @@
+{
+    'name': "Manager Football",
+    'name_vi_VN': "Quản lý đội bóng",
+
+    'summary': """
+Manager the football
+    """,
+    'summary_vi_VN': """
+Quản lý đội bóng
+    """,
+
+    'description': """
+What it does
+============
+* This module allow manager the football
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+    """,
+
+    'description_vi_VN': """
+Ứng dụng này làm gì
+===================
+* Module này cho phép quản lý đội bóng
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+    """,
+
+    'author': "TNS - LoveViruss",
+    'support': "transon821996@gmail.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/tvtma/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Football',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [
+    ],
+    'images' : [
+        # 'static/description/main_screenshot.png'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 99.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
