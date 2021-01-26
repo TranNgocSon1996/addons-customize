97cd517c5ba6bf33bb3eefc6d7fea2c566c3b16a
TranNgocSon1996 <transon821996@gmail.com>
2021-01-26 10:39:21 +0700

    [IMP] tns_football_management: link player to football club

--- tns_football_management/models/football_club.py
+++ tns_football_management/models/football_club.py
@@ -1,8 +1,20 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class FootballClub(models.Model):
     _name = 'football.club'
+    _inherit = 'image.mixin'
+    _order = 'id'
     _description = 'Football Club'
     
     name = fields.Char(string='Name', required=True)
+    nickname = fields.Char(string='Nickname')
+    coach_id = fields.Many2one('res.partner', string='Coach')
+    captain_id = fields.Many2one('res.partner', string='Captain', required=True)
+    player_total = fields.Integer(string='Player Total', compute='_compute_player_total', stored=True, default=0)
+    player_ids = fields.One2many('res.partner', inverse_name='club_id', string="Player")
+    
+    @api.depends('player_ids')
+    def _compute_player_total(self):
+        for r in self:
+            r.player_total = len(r.player_ids)
--- tns_football_management/models/partner.py
+++ tns_football_management/models/partner.py
@@ -3,3 +3,5 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    
+    club_id = fields.Many2one('football.club', string="Football Club")
